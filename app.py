c596842b4563a981fea556b82cfeb659be984cf4
WhisperedShadow <dd9514371@gmail.com>
2025-03-14 21:55:27 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,174 @@
+from flask import Flask, render_template, request, redirect, url_for, flash, Response
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from database import db, User, KeyBase, Keys
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "Arandomsecretkey"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message = 'You need to be logged in to access this page'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('index'))
+        else:
+            flash('Invalid username or password')
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        if User.query.filter_by(username=username).first():
+            flash('User already exists')
+        else:
+            new_user = User(
+                username=username, 
+                email=email, 
+                password=generate_password_hash(password, method='pbkdf2:sha256')
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect(url_for('login'))
+    return render_template('register.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/')
+@login_required
+def index():
+    return render_template('index.html', bases=KeyBase.query.filter_by(user_id=current_user.id).all())
+
+@app.route('/new', methods=['GET', 'POST'])
+@login_required
+def new():
+    if request.method == 'POST':
+        category = request.form.get('category')
+        
+        if not category:
+            flash("Category is required")
+            return redirect(url_for('new'))
+        
+        new_base = KeyBase(base_name=category, user_id=current_user.id)
+        db.session.add(new_base)
+        db.session.commit()  
+
+        for key, value in request.form.items():
+            if key.startswith("key") and value:
+                index = key[3:]  
+                keyname = value
+                keyvalue = request.form.get(f'value{index}')
+                
+                if keyvalue: 
+                    new_key = Keys(base_id=new_base.base_id, keyname=keyname, key=keyvalue)
+                    db.session.add(new_key)
+
+        db.session.commit()  
+        return redirect(url_for('index'))
+        
+    return render_template('new.html')
+
+@app.route('/remove/<int:base_id>')
+@login_required
+def deletebase(base_id):
+    base = KeyBase.query.get_or_404(base_id) 
+    Keys.query.filter_by(base_id=base_id).delete()
+    db.session.delete(base)
+    db.session.commit()
+    return redirect(url_for('index'))
+
+@app.route('/edit/<int:base_id>', methods=['GET', 'POST'])
+@login_required
+def edit(base_id):
+    base = KeyBase.query.get_or_404(base_id)
+    keys = Keys.query.filter_by(base_id=base_id).all()
+
+    if request.method == 'POST':
+        category = request.form.get('category')
+        
+        if not category:
+            flash("Category is required")
+            return redirect(url_for('edit', base_id=base_id))
+        
+        base.base_name = category
+        db.session.commit()
+
+        existing_keys = {k.keyname: k for k in keys}
+
+        new_keys = {}
+        for key, value in request.form.items():
+            if key.startswith("key") and value:
+                index = key[3:]  
+                keyname = value
+                keyvalue = request.form.get(f'value{index}')
+                
+                if keyvalue:
+                    new_keys[keyname] = keyvalue
+        for existing_keyname in list(existing_keys.keys()):
+            if existing_keyname not in new_keys:
+                db.session.delete(existing_keys[existing_keyname])
+
+        for keyname, keyvalue in new_keys.items():
+            if keyname in existing_keys:
+                existing_keys[keyname].key = keyvalue
+            else:
+                db.session.add(Keys(base_id=base.base_id, keyname=keyname, key=keyvalue))
+
+        db.session.commit()
+        return redirect(url_for('index'))
+        
+    return render_template('edit.html', base=base, keys=keys)
+
+@app.route('/export', methods=['GET', 'POST'])
+@login_required
+def export():
+    bases = KeyBase.query.filter_by(user_id=current_user.id).all()
+
+    if request.method == 'POST':
+        selected_bases = request.form.getlist('base')  
+        prefix = request.form.get('prefix', '') 
+        keys = Keys.query.filter(Keys.base_id.in_(selected_bases)).all()
+        formatted_keys = []
+        for key in keys:
+            keyname = key.keyname.upper()
+            if prefix == "react":
+                keyname = f"REACT_APP_{keyname}"
+            elif prefix == "vite":
+                keyname = f"VITE_{keyname}"
+            formatted_keys.append(f"{keyname}={key.key}")
+        env_content = "\n".join(formatted_keys)
+        return Response(
+            env_content,
+            mimetype="text/plain",
+            headers={"Content-Disposition": "attachment; filename=.env"}
+        )
+
+    return render_template('export.html', bases=bases)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ database.py
@@ -0,0 +1,42 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+    bases = db.relationship('KeyBase', backref='user', lazy=True)
+
+    def __repr__(self):
+        return f'User <{self.username}>'
+
+
+class KeyBase(db.Model):
+    __tablename__ = "bases"
+
+    base_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    base_name = db.Column(db.String(100), unique=True, nullable=False)
+
+    keys = db.relationship('Keys', backref='base', lazy=True)
+
+    def __repr__(self):
+        return f'KeyBase <{self.base_name}>'
+
+
+class Keys(db.Model):
+    __tablename__ = "keys"
+
+    id = db.Column(db.Integer, primary_key=True)
+    base_id = db.Column(db.Integer, db.ForeignKey('bases.base_id'), nullable=False)
+    keyname = db.Column(db.String(100), nullable=False)
+    key = db.Column(db.String(255), nullable=False) 
+
+    def __repr__(self):
+        return f'Key <{self.keyname}>'
